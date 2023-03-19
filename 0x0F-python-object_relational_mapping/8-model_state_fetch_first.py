1bcad8c7291a9107f27bfec8c9646750c0dba8a2
Blessing-C <chikaodili07@gmail.com>
2023-03-19 16:09:11 -0700

    updated

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,20 +1,24 @@
-#!/usr/bin/python3
-""" prints the first State object from the database hbtn_0e_6_usa
+i#!/usr/bin/python3
+"""
+a script that prints the first State object from the database
+hbtn_0e_6_usa
 """
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
 from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
 
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+if __name__ == '__main__':
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(sys.argv[1], sys.argv[2], sys.argv[3])
+    )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).first()
-    if instance is None:
-        print("Nothing")
+    res = session.query(State).first()
+    if res:
+        print("{}: {}".format(res.id, res.name))
     else:
-        print(instance.id, instance.name, sep=": ")
+        print("Nothing")
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
-Contains State class and Base, an instance of declarative_base()
+a python file that contains the class definition
+of a State and an instance Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-mymetadata = MetaData()
-Base = declarative_base(metadata=mymetadata)
+my_metadata = MetaData()
+Base = declarative_base(metadata=my_metadata)
 
 
 class State(Base):
@@ -14,5 +15,10 @@ class State(Base):
     Class with id and name attributes of each state
     """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        unique=True
+    )
     name = Column(String(128), nullable=False)
